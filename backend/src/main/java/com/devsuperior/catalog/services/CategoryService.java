1b52978ce257e3ebe420c09a9cb2e9791b221d7e
jocile <jocile@gmail.com>
2022-07-11 16:23:45 -0300

    :recycle: refactor(service): changes using lambda service changes using lambda function to map a stream list

--- backend/src/main/java/com/devsuperior/catalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/catalog/services/CategoryService.java
@@ -3,8 +3,8 @@ package com.devsuperior.catalog.services;
 import com.devsuperior.catalog.dto.CategoryDTO;
 import com.devsuperior.catalog.entities.Category;
 import com.devsuperior.catalog.repositories.CategoryRepository;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,11 +18,9 @@ public class CategoryService {
   public List<CategoryDTO> findAll() {
     List<Category> list = repository.findAll();
 
-    List<CategoryDTO> listDto = new ArrayList<>();
-    for (Category entity : list) {
-      listDto.add(new CategoryDTO(entity));
-    }
-
-    return listDto;
+    return list
+      .stream()
+      .map(obj -> new CategoryDTO(obj))
+      .collect(Collectors.toList());
   }
 }
