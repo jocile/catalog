15f71f481bf817dfbf4d9884c2a4d6e39ef2fac1
jocile <jocile@gmail.com>
2022-09-01 19:05:07 -0300

    :recycle: refac: relationship products in category

--- backend/src/main/java/com/devsuperior/catalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/catalog/entities/Category.java
@@ -2,11 +2,14 @@ package com.devsuperior.catalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -39,6 +42,10 @@ public class Category implements Serializable {
   @Setter(value = AccessLevel.NONE)
   @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant updatedAt;
+
+  @Setter(value = AccessLevel.NONE)
+  @ManyToMany(mappedBy = "categories")
+  private Set<Product> products = new HashSet<>();
 
   @PrePersist
   public void prePersist() {
