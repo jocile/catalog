09a5b71fc87dd2677704ad0e4171752c22baef6f
jocile <jocile@gmail.com>
2022-09-02 17:52:54 -0300

    :card_file_box: database: ORM mapping User Role

--- backend/src/main/java/com/devsuperior/catalog/entities/Role.java
+++ backend/src/main/java/com/devsuperior/catalog/entities/Role.java
@@ -1,6 +1,11 @@
 package com.devsuperior.catalog.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +13,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable {
   private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String authority;
 }
--- backend/src/main/java/com/devsuperior/catalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/catalog/entities/User.java
@@ -3,6 +3,14 @@ package com.devsuperior.catalog.entities;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -15,10 +23,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String firstName;
@@ -27,5 +39,11 @@ public class User implements Serializable {
   private String password;
 
   @Setter(value = AccessLevel.NONE)
+  @ManyToMany
+  @JoinTable(
+    name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
   private Set<Role> roles = new HashSet<>();
 }
