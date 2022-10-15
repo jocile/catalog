628fedcd31c28b46b75a741d2c9823f432c8bc45
jocile <jocile@gmail.com>
2022-10-15 19:35:32 -0300

    :sparkles: feat: Token additional information

--- backend/src/main/java/com/devsuperior/catalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/catalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.catalog.config;
 
+import com.devsuperior.catalog.components.JwtTokenEnhancer;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +12,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -42,7 +42,7 @@ public class AuthorizationServerConfig
   private AuthenticationManager authenticationManager;
 
   @Autowired
-  private TokenEnhancer tokenEnhancer;
+  private JwtTokenEnhancer tokenEnhancer;
 
   @Override
   public void configure(AuthorizationServerSecurityConfigurer security)
